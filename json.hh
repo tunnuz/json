e4acf38d6c1533d7bab9c549ed221c6e4472670e
tunnuz <tunnuz@gmail.com>
2014-02-01 13:31:04 +0100

    Porting to CMake finished.

--- json.hh
+++ json.hh
@@ -2,7 +2,7 @@
 #define JSON_HH
 
 #include "json_st.hh" // JSON syntax tree
-#include "json.tab.h" // parser
+#include "json.tab.hh" // parser
   
 JSON::Value parse_file(const char* filename);
 JSON::Value parse_string(const std::string& s);
